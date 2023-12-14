7a44fee153869bdee089480ff75b98ab83c7342d
Vitaliy Alekseevich <vit.k2000@yandex.ru>
2023-12-14 10:40:26 +0500

    first commit

---
+++ src/main/java/Kukushkin/sklad/SkladApplication.java
@@ -0,0 +1,13 @@
+package Kukushkin.sklad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkladApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kukushkin/sklad/SkladApplicationTests.java
@@ -0,0 +1,13 @@
+package Kukushkin.sklad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkladApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
